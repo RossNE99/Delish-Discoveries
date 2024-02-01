a3f5c24558706f81288efb9415ccbb89be8f9382
Obaid Gulzar <obaid3337@googlemail.com>
2024-02-01 20:44:33 +0000

    function set up tp pull data from API

--- Assets/js/chart.js
+++ Assets/js/chart.js
@@ -1,22 +1,30 @@
+const ctx = document.getElementById('myChart');
 
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
+new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Protein', 'Carbohydrate', 'Fat', 'Fiber', 'Sugar', 'Salt'],
-      datasets: [{
-        label: 'Grams',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+        labels: ['Protein', 'Carbohydrate', 'Fat', 'Fiber', 'Sugar', 'Salt'],
+        datasets: [{
+            label: 'Grams',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
+        scales: {
+            y: {
+                beginAtZero: true
+            }
         }
-      }
     }
-  });
+});
 
+
+fetchData(testIngredients.join(","), renderRecipeList, notworked);
+
+function renderRecipeList(data) {
+    if (data && data.count) {
+        console.log(data);
+    }
+    console.log(data.hits[0].recipe.calories);
+}
