782e40f3056db443d6e45a7bfb03acdf4a4aff57
Obaid Gulzar <obaid3337@googlemail.com>
2024-02-01 20:45:11 +0000

    all nutrients data ploted on chart

--- Assets/js/chart.js
+++ Assets/js/chart.js
@@ -1,30 +1,39 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart'); //gets chart by ID from html page
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Protein', 'Carbohydrate', 'Fat', 'Fiber', 'Sugar', 'Salt'],
-        datasets: [{
-            label: 'Grams',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+fetchData(testIngredients.join(","), renderGraph, notworked);
 
 
-fetchData(testIngredients.join(","), renderRecipeList, notworked);
-
-function renderRecipeList(data) {
+function renderGraph(data) {
     if (data && data.count) {
         console.log(data);
     }
     console.log(data.hits[0].recipe.calories);
+   
+    var protein = data.hits[0].recipe.totalNutrients.PROCNT;
+    var carbs = data.hits[0].recipe.totalNutrients.CHOCDF;
+    var fat = data.hits[0].recipe.totalNutrients.FAT;
+    var fiber = data.hits[0].recipe.totalNutrients.FIBTG;
+    var sugars = data.hits[0].recipe.totalNutrients.SUGAR;
+    var sodium = data.hits[0].recipe.totalNutrients.NA;
+
+
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Protein', 'Carbs', 'Fat', 'Fiber', 'Sugars', 'Sodium'],
+          datasets: [{
+              label: 'Grams',
+              data: [protein.quantity, carbs.quantity, fat.quantity, fiber.quantity, sugars.quantity, sodium.quantity/1000],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
 }
