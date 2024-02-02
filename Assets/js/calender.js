51306105b975626d3fecfc9dbf7b23f6bf22b7e9
Obaid Gulzar <obaid3337@googlemail.com>
2024-02-02 19:19:34 +0000

    using day.js to add to todays date

--- Assets/js/calender.js
+++ Assets/js/calender.js
@@ -11,55 +11,61 @@ document.addEventListener('DOMContentLoaded', function() {
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2023-11-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2023-11-07',
-          end: '2023-11-10'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2023-11-09T16:00:00'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2023-11-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2023-11-11',
-          end: '2023-11-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-11-12T10:30:00',
-          end: '2023-11-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2023-11-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2023-11-12T14:30:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2023-11-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'https://google.com/',
-          start: '2023-11-28'
-        }
-      ]
+      events: eventsArray
     });
   
     calendar.render();
   });
+
+  eventsArray = [
+    {
+      title: 'All Day Event',
+      start: '2023-11-01'
+    },
+    {
+        title: 'All Day Event',
+        start: '2023-11-01'
+      },
+    {
+      title: 'Long Event',
+      start: '2023-11-07',
+      end: '2023-11-10'
+    },
+    {
+      groupId: '999',
+      title: 'Repeating Event',
+      start: '2023-11-09T16:00:00'
+    },
+    {
+      title: 'Meeting',
+      start: '2023-11-12T14:30:00'
+    },
+    {
+      title: 'Birthday Party',
+      start: '2023-11-13T07:00:00'
+    },
+    {
+        title: 'Birthday Party',
+        start: '2023-11-13T07:00:00'
+      },
+    {
+        title: 'Rice-Milk Rice Pudding', 
+        start:  '2024-02-02'
+    }
+    
+  ];
+
+var addToCalenderBtn = $("#addToCalendarBtn");
+
+addToCalenderBtn.on("click", writeOnCalendar)
+
+function writeOnCalendar() {
+    eventsArray.push(
+        {
+            title: "Rice-Milk Rice Pudding", 
+            start:  dayjs().format'(YYYY-MM-DD')
+        }
+    )
+}
+
+console.log(dayjs().format'('YYYY-MM-DD'));
