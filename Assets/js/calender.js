399c7c35d3651da69be5dcac15f49712cb45149c
Obaid Gulzar <obaid3337@googlemail.com>
2024-02-02 20:08:43 +0000

    removed commenting out

--- Assets/js/calender.js
+++ Assets/js/calender.js
@@ -1,47 +1,46 @@
 //initialView: 'dayGridWeek' //timeGridDay, dayGridWeek, listWeek, dayGridMonth
 
-console.log(data.recipe.totalNutrients.PROCNT);
+//console.log(data.recipe.totalNutrients.PROCNT);
 
 // addToCalenderBtn.on("click", 
 
 // function renderCalendar(data) {
 //     if (!data)  return
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var calendarEl = document.getElementById('calendar');
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
   
-//     var calendar = new FullCalendar.Calendar(calendarEl, {
-//       initialView: 'dayGridMonth',
-//       initialDate: dayjs().format('YYYY-MM-DD'),
-//       headerToolbar: {
-//         left: 'prev,next today',
-//         center: 'title',
-//         right: 'dayGridMonth,timeGridWeek,timeGridDay'
-//       },
-//       events: eventsArray
-//     });
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      initialDate: dayjs().format('YYYY-MM-DD'),
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      events: eventsArray
+    });
   
-//     calendar.render();
-//   });
+    calendar.render();
+  });
 
-//   eventsArray = [
-//     {
-//         title: //data.recipe.label, 
-//         start:  dayjs().format('YYYY-MM-DD')
-//     }
-//   ];
-
-
-// var addToCalenderBtn = $("#addToCalendarBtn");
+  eventsArray = [
+    {
+        title: "Name Of Item",//data.recipe.label 
+        start:  dayjs().format('YYYY-MM-DD')
+    }
+  ];
 
 
+var addToCalenderBtn = $("#addToCalendarBtn");
 
-// function writeOnCalendar() {
-//     eventsArray.push(
-//         {
-//             title: "Rice-Milk Rice Pudding", 
-//             start:  dayjs().format('YYYY-MM-DD')
-//         }
-//     )
-// }
-// });
+
+
+function writeOnCalendar() {
+    eventsArray.push(
+        {
+            title: "Rice-Milk Rice Pudding", 
+            start:  dayjs().format('YYYY-MM-DD')
+        }
+    )
+};
