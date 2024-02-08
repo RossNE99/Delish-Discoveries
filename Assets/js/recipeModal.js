7bb6fc5a6fb552d36af1b8e7e49aa08c35aaf99d
Ross <rossycharlton@googlemail.com>
2024-02-08 14:27:46 +0000

    Added more comments to recipeModal.js

--- Assets/js/recipeModal.js
+++ Assets/js/recipeModal.js
@@ -1,20 +1,20 @@
 function openRecipeModal(e, recipe) {
 
-    $("#recipeModal button").off("click");
+    $("#recipeModal button").off("click");  //Remove all click event listners from old/prev modal
 
-    if(!recipe){
-        var recipeId = $(e.currentTarget).data("recipeid")
-        recipe = loadedRecipes.find(recipe => recipe._links.self.href === recipeId);    //This probs inst tbe most efficent way but just send it
+    if(!recipe){ //If no recipe was passed into this function then it was called from the search page, below is how we find the correct recipe that the user clicked on
+        var recipeId = $(e.currentTarget).data("recipeid")  //Get the uniqe id for the recipe that was clicked
+        recipe = loadedRecipes.find(recipe => recipe._links.self.href === recipeId);    //retrive that recipe from the loadedrecipe array, using recipeID to identify the correct one 
     } else {
       //  $("#planMealDateInput").addClass - Unfinnished remove buttons for add to call here if opened from cal
     }
 
 
-    var {image, label, totalTime, calories, dietLabels, cuisineType, dishType, yield, ingredientLines, url} = recipe.recipe
-    $("#recipeModalLabel").text(label)
-    var modalImg = $("<img>", {src: image, alt: label, style: "object-fit: contain; height: 100% ;width: 100%;"})
-    $("#recipeModalBodyImg").empty().append(modalImg)
-    var recipeInfo =$("<div>", {
+    var {image, label, totalTime, calories, dietLabels, cuisineType, dishType, yield, ingredientLines, url} = recipe.recipe //Destructute recipe object
+    $("#recipeModalLabel").text(label)  //set modal heading to the name of the recipe
+    var modalImg = $("<img>", {src: image, alt: label, style: "object-fit: contain; height: 100% ;width: 100%;"})   //create image of the recipe to the modal
+    $("#recipeModalBodyImg").empty().append(modalImg) //add the image into the modal
+    var recipeInfo =$("<div>", {    //create the recipe info section
         html: ` 
         <div class="card-header">
            <h5>Recipe Info</h5>
@@ -28,39 +28,39 @@ function openRecipeModal(e, recipe) {
             <li class="list-group-item"><b>serves:</b> ${yield} People</li>
         </ul>`    //Not sure if yield is acuareate :/
     })
-    $("#recipeInfo").empty().append(recipeInfo)
+    $("#recipeInfo").empty().append(recipeInfo) //add the recipe info section to the page
 
     $("#ingredientsList").empty()
-    $.each(ingredientLines, function (indexInArray, ingredient) { 
+    $.each(ingredientLines, function (indexInArray, ingredient) { //create ingredint list
         var ingredientLI = $("<li>", {
             class:"list-group-item" ,
             text: ingredient,
         })
-        $("#ingredientsList").append(ingredientLI)
+        $("#ingredientsList").append(ingredientLI)  //add each ingreadint to the page
     });
 
-    $("#viewCookingInstructionsBtn").on('click', function(){ window.open(url, '_blank')});
+    $("#viewCookingInstructionsBtn").on('click', function(){ window.open(url, '_blank')});  //open recipe instructions in new tab when view recipe button is clicked
 
-    renderGraph(recipe)
-    $('#recipeModal').modal('show');
+    renderGraph(recipe) //create the chart
+    $('#recipeModal').modal('show');    //add the chart to the page 
 
-    $('#planMealDateInput').daterangepicker({
+    $('#planMealDateInput').daterangepicker({   //event listerner for date picker
         drops: "up",
         autoUpdateInput: true,
-        singleDatePicker: true,
+        singleDatePicker: true,         //date picker options
         showDropdowns: true,
         autoApply: true,
-      }, function(mealDay){ savePlannedRecipeToLocalStorage(mealDay)}
+      }, function(mealDay){ savePlannedRecipeToLocalStorage(mealDay)}   //callback function is called one user selects a day to plan meal for
     )
 
-    $("#planMealForToday").on("click", function() {savePlannedRecipeToLocalStorage(dayjs())})
+    $("#planMealForToday").on("click", function() {savePlannedRecipeToLocalStorage(dayjs())}) //if planForTodaybtn is pressed then save the meal for the current day
 
-    function savePlannedRecipeToLocalStorage(mealDay) {
-        var PlannedMealObj = {mealDay: mealDay.format("YYYY-MM-DD"), recipe}
-        var prevPlannedMeals = JSON.parse(localStorage.getItem("plannedMeals")) || []
+    function savePlannedRecipeToLocalStorage(mealDay) { //function to handel saving the recipe
+        var PlannedMealObj = {mealDay: mealDay.format("YYYY-MM-DD"), recipe}    //create the plannedMeal object
+        var prevPlannedMeals = JSON.parse(localStorage.getItem("plannedMeals")) || []   //get local stoarage, if there is none then retuen an empty array
     
-        localStorage.setItem("plannedMeals", JSON.stringify([...prevPlannedMeals, PlannedMealObj]))
-        showToast("Meal added to calendar!", `${recipe.recipe.label}, has been planned for ${dayjs(mealDay).format("D MMM")}`, `fa-save`, 'blue') //show a toast to the user to let them know that there task has been saved
+        localStorage.setItem("plannedMeals", JSON.stringify([...prevPlannedMeals, PlannedMealObj])) //save the new meal object into local storage
+        showToast("Meal added to calendar!", `${recipe.recipe.label}, has been planned for ${dayjs(mealDay).format("D MMM")}`, `fa-save`, 'blue') //show a toast to the user to let them know that there meal has been saved
         }
         
       // Event listener for the "Add to Favorites" button
@@ -100,7 +100,7 @@ function addToFavorites(recipe) {
 
     favorites.push(recipe);
     localStorage.setItem("favorites", JSON.stringify(favorites));
-    showToast("Meal Saved!", `${recipe.recipe.label}, has been saved to favs`, `fa-save`, 'yellow') //show a toast to the user to let them know that there task has been saved
+    showToast("Meal Saved!", `${recipe.recipe.label}, has been saved to favs`, `fa-save`, 'yellow') //show a toast to the user to let them know that there meal has been saved
   
     function favExists(href, prevFavs) {        
         return prevFavs.some(obj =>
